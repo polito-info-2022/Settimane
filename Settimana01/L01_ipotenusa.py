59352ee6bda479971b0185f1c61a945739b8eea6
Fulvio Corno <fulvio.corno@polito.it>
2022-09-26 18:03:02 +0200

    Materiale lezioni L01c e L3

---
+++ Settimana01/L01_ipotenusa.py
@@ -0,0 +1,13 @@
+# Programma introduttivo di esempio
+
+import math
+
+# Cateti
+a = 3.0
+b = 4.0
+
+# Calcolo ipotenusa
+
+c = math.sqrt(a*a + b*b)
+
+print('Il triangolo con cateti', a, b, 'ha ipotenusa pari a', c)
