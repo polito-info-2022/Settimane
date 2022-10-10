531909daae422d17fb1913631feb3b76af5c6910
Fulvio Corno <fulvio.corno@polito.it>
2022-10-10 15:07:52 +0200

    Testo esercizio

---
+++ Settimana03/piastrelle.py
@@ -0,0 +1,5 @@
+# Bisogna posizionare lungo il muro una fila di piastrelle nere e bianche.
+# Per ragioni estetiche l’architetto ha specificato che la prima e l’ultima piastrella devono essere nere.
+# Il vostro compito è calcolare il numero di piastrelle necessarie e il vuoto a ciascuna delle due estremità della
+# riga, dato lo spazio disponibile e la larghezza di ogni piastrella.
+
