b1c805ad05624f886e70a0453dd94d7bb959d7a4
Fulvio Corno <fulvio.corno@polito.it>
2022-10-17 17:28:24 +0200

    If annidati vs operatori booleani

---
+++ Settimana04/compreso.py
@@ -0,0 +1,24 @@
+val_min = 10
+val_max = 20
+
+numero = int(input(f"Dammi un numero tra {val_min} e {val_max}: "))
+
+if numero < val_min:
+    print('Escluso')
+elif numero > val_max:
+    print('Escluso')
+else:
+    print('Compreso')
+
+if numero >= val_min:
+    if numero <= val_max:
+        print('Compreso')
+    else:
+        print('Escluso')
+else:
+    print('Escluso')
+
+if numero >= val_min and numero <= val_max:
+    print('Compreso')
+else:
+    print('Escluso')
